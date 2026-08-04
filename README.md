ab80e4323636c56c8aef9452f6d1f9004d5c1fa7
GoobusTheNoobus <huand2464@wrdsb.ca>
2026-08-04 12:54:15 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Yuzu
+Yuzu is a LLVM based compiled programming language. It is also the name of a citrus fruit.
---
+++ source/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <llvm/IR/Module.h>
+
+int main() {
+    std::cout << "Hello Yuzu\n";
+    return 0;
+}
